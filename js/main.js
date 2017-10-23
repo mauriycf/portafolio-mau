c837900d5ffdcf581dc3c1b8b1fb45b199fa0e9c
mauriycf <mauriycf@gmail.com>
2017-10-23 09:31:58 -0400

    Add files via upload
    listp

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Declarar la funcion
+/*
+var numero1 = parseFloat(prompt('Ingrese un valor: '));
+var numero2 = parseFloat(prompt('Ingrese un valor: '));
+
+function sumar(numero1,numero2){
+  var resultado = numero1+numero2;
+  document.write(resultado);
+}
+
+sumar(numero1,numero2);
+*/
+
+/* hamburger button */
+
+$('#wrapper').click( function(){
+  $('.menu').toggleClass('close');
+  $('.sidebar').toggleClass('activo');
+});
+
+/*----------------------*/
